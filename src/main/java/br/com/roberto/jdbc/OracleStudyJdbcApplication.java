cb3623ae88eee58ba039e99c03db3f17740b704c
jrpbjr <jrpbjr@gmail.com>
2020-11-18 21:38:59 -0300

    commit adicional, configurando github

---
+++ src/main/java/br/com/roberto/jdbc/OracleStudyJdbcApplication.java
@@ -0,0 +1,13 @@
+package br.com.roberto.jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OracleStudyJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OracleStudyJdbcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/roberto/jdbc/OracleStudyJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.roberto.jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OracleStudyJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
